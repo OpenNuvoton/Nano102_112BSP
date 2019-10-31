172f0615be1e5d809cfca0ac2d313d9c570193a3
mjchen <mjchen@nuvoton.com>
2019-10-31 09:44:39 +0800

    add ISP I2C/RS485 sample
    Change-Id: I1870c4119ec5f391dec12bb3c060b880c2d2971d

---
+++ SampleCode/ISP/ISP_I2C/main.c
@@ -0,0 +1,103 @@
+/***************************************************************************//**
+ * @file     main.c
+ * @brief    Transmit and receive ISP command from PC terminal through RS232 interface
+ * @version  2.0.0
+ *
+ * @copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include "Nano1X2Series.h"
+#include "targetdev.h"
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Define functions prototype                                                                              */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void);
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+    /* Enable HIRC */
+    CLK->PWRCTL |= CLK_PWRCTL_HIRC_EN_Msk | CLK_PWRCTL_HXT_EN_Msk;
+    /* Waiting for HIRC clock ready */
+    CLK_WaitClockReady(CLK_PWRCTL_HIRC_EN_Msk);
+    CLK_EnablePLL(CLK_PLLCTL_PLL_SRC_HIRC, 32000000);
+    CLK_WaitClockReady(CLK_CLKSTATUS_PLL_STB_Msk);
+    CLK_SetHCLK(CLK_CLKSEL0_HCLK_S_PLL, CLK_HCLK_CLK_DIVIDER(1));
+
+    SystemCoreClock = 32000000;     // HCLK
+
+    /* Enable IP clock */
+    CLK->APBCLK |= CLK_APBCLK_I2C1_EN; // I2C1 Clock Enable
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Set PB multi-function pins for UART0 RXD and TXD  */
+    SYS->PA_H_MFP &= ~(SYS_PA_H_MFP_PA14_MFP_Msk | SYS_PA_H_MFP_PA15_MFP_Msk);
+    SYS->PA_H_MFP |= (SYS_PA_H_MFP_PA14_MFP_I2C1_SCL | SYS_PA_H_MFP_PA15_MFP_I2C1_SDA);
+    /* Lock protected registers */
+    // SYS_LockReg();
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* MAIN function                                                                                           */
+/*---------------------------------------------------------------------------------------------------------*/
+
+int main(void)
+{
+    uint32_t cmd_buff[16];
+    /* Init System, IP clock and multi-function I/O */
+    SYS_Init();
+    /* Init I2C for printf */
+    I2C_Init();
+    CLK->AHBCLK |= CLK_AHBCLK_ISP_EN_Msk;
+    FMC->ISPCON |= FMC_ISPCON_ISPEN_Msk;
+    g_apromSize = GetApromSize();
+    GetDataFlashInfo(&g_dataFlashAddr, &g_dataFlashSize);
+    SysTick->LOAD = 300000 * CyclesPerUs;
+    SysTick->VAL   = (0x00);
+    SysTick->CTRL = SysTick->CTRL | SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;//using cpu clock
+
+    while (1)
+    {
+        if (bI2cDataReady == 1)
+        {
+            goto _ISP;
+        }
+
+        //if((SysTick->CTRL & (1 << 16)) != 0)//timeout, then goto APROM
+        if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)
+        {
+            goto _APROM;
+        }
+    }
+
+_ISP:
+
+    while (1)
+    {
+        if (bI2cDataReady == 1)
+        {
+            memcpy(cmd_buff, i2c_rcvbuf, 64);
+            bI2cDataReady = 0;
+            ParseCmd((unsigned char *)cmd_buff, 64);
+            NVIC_EnableIRQ(I2C1_IRQn);
+        }
+    }
+
+_APROM:
+    outpw(&SYS->RST_SRC, 3);//clear bit
+    outpw(&FMC->ISPCON, inpw(&FMC->ISPCON) & 0xFFFFFFFC);
+    outpw(&SCB->AIRCR, (V6M_AIRCR_VECTKEY_DATA | V6M_AIRCR_SYSRESETREQ));
+
+    /* Trap the CPU */
+    while (1);
+}
+
---
+++ SampleCode/ISP/ISP_I2C/targetdev.h
@@ -0,0 +1,14 @@
+/***************************************************************************//**
+ * @file     targetdev.c
+ * @brief    ISP support function header file
+ * @version  2.0.0
+ *
+ * @copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+
+#include "Nano1X2Series.h"
+#include "i2c_transfer.h"
+#include "ISP_USER.h"
+
+#define I2C_ADDR 0x60
+
